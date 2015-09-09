5edefe49b3a8d9ec92fa5627f87024afaed9c6fd
Esko Luontola <esko.luontola@gmail.com>
2015-09-09 23:16:34 +0300

    Change the page title per page

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require 'sinatra'
 
 get '/' do
-  erb :index
+  erb :index, :locals => {:title => 'I\'m Ruby'}
 end
 
 get '/pictures.html' do
   picture_urls = Dir['public/pictures/**'].map { |path| path.sub('public', '') }
-  erb :pictures, :locals => {:picture_urls => picture_urls}
+  erb :pictures, :locals => {:picture_urls => picture_urls, :title => 'Lovely Pictures'}
 end
 
 # must be last; this will be used if none of the above matches the request
 get '/:page.html' do |page|
-  erb page.to_sym
+  erb page.to_sym, :locals => {:title => '???'}
 end
