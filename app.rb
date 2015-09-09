480bf7c0cea7183fb26a42c9ac6f9f186b9d0e3b
Esko Luontola <esko.luontola@gmail.com>
2015-09-09 22:36:44 +0300

    Proper templating system

--- app.rb
+++ app.rb
@@ -5,7 +5,5 @@ get '/' do
 end
 
 get '/:page.html' do |page|
-  IO.read('views/layout-top.html') +
-  IO.read('views/' + page + '.html') +
-  IO.read('views/layout-bottom.html')
+  erb page.to_sym
 end
