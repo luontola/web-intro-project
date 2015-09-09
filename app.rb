1ed8c59fea1452a8f6b6802b5408c690318eb198
Esko Luontola <esko.luontola@gmail.com>
2015-09-09 22:26:35 +0300

    Minimal template system

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ get '/' do
   redirect to('/my-page.html')
 end
 
-get '/:page' do |page|
-  IO.read('public/' + page + '.html')
+get '/:page.html' do |page|
+  IO.read('views/layout-top.html') +
+  IO.read('views/' + page + '.html') +
+  IO.read('views/layout-bottom.html')
 end
