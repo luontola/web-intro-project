f63a6cc1c01afe211d9f755ba0c86f69a5ae1e09
Esko Luontola <esko.luontola@gmail.com>
2015-09-30 00:06:13 +0300

    Sitemap for titles

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 require 'sinatra'
 
+PAGES = {
+  :index => "I'm Ruby",
+  :pictures => "Lovely Pictures",
+  :story => "My Story",
+}
+
 get '/' do
-  erb :index, :locals => {:title => 'I\'m Ruby'}
+  erb :index, :locals => {:title => PAGES[:index]}
 end
 
 get '/pictures.html' do
   picture_urls = Dir['public/pictures/**'].map { |path| path.sub('public', '') }
-  erb :pictures, :locals => {:picture_urls => picture_urls, :title => 'Lovely Pictures'}
+  erb :pictures, :locals => {:picture_urls => picture_urls, :title => PAGES[:pictures]}
 end
 
 # must be last; this will be used if none of the above matches the request
 get '/:page.html' do |page|
-  erb page.to_sym, :locals => {:title => '???'}
+  erb page.to_sym, :locals => {:title => PAGES[page.to_sym]}
 end
