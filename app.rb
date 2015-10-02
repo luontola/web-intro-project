ef3cfe65226c4437fed66addcad1d765ddd103af
Esko Luontola <esko.luontola@gmail.com>
2015-10-03 00:35:13 +0300

    Front page at the root

--- app.rb
+++ app.rb
@@ -1,10 +1,6 @@
 require 'sinatra'
 
 get '/' do
-  redirect to('/about.html')
-end
-
-get '/about.html' do
   backstreet_boys = ["A.J.", "Howie", "Nick", "Kevin", "Brian"]
   @who_i_marry = backstreet_boys.sample
   erb :about
