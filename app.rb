ee2a9760fbfa3dac04780c2294430a3bcebf3214
Esko Luontola <esko.luontola@gmail.com>
2015-10-03 00:52:17 +0300

    Sitemap for titles

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra'
 
+PAGES = {
+  :index => "I'm Ruby",
+  :pictures => "Lovely Pictures",
+  :story => "My Story",
+}
+
 get '/' do
-  erb :index, :locals => {:title => "I'm Ruby"}
+  erb :index, :locals => {:title => PAGES[:index]}
 end
 
 get '/pictures.html' do
   picture_urls = Dir.glob('public/pictures/**').map { |path| path.sub('public', '') }
-  erb :pictures, :locals => {:picture_urls => picture_urls, :title => "Lovely Pictures"}
+  erb :pictures, :locals => {:picture_urls => picture_urls, :title => PAGES[:pictures]}
 end
 
 get '/:page.html' do
-  erb params['page'].to_sym, :locals => {:title => '???'}
+  page = params['page'].to_sym
+  erb page, :locals => {:title => PAGES[page]}
 end
