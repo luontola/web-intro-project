887bfac0ebb3eff948aed1ce40bc7fda70026eec
Esko Luontola <esko@luontola.fi>
2015-10-03 00:46:02 +0300

    Unique titles for every page

--- app.rb
+++ app.rb
@@ -2,12 +2,14 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
+  @title = "I'm Ruby"
   backstreet_boys = ["A.J.", "Howie", "Nick", "Kevin", "Brian"]
   @who_i_marry = backstreet_boys.sample
   erb :about
 end
 
 get '/pictures.html' do
+  @title = "Lovely Pictures"
   @picture_urls = Dir.glob('public/pictures/**').map { |path| path.sub('public', '') }
   erb :pictures
 end
