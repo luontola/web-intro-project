d94fc393487def0e1e1421166b8b3b749774c418
Esko Luontola <esko.luontola@gmail.com>
2015-10-03 00:15:04 +0300

    Generating pages dynamically

--- app.rb
+++ app.rb
@@ -3,3 +3,11 @@ require 'sinatra'
 get '/' do
   redirect to('/my-page.html')
 end
+
+get '/foo' do
+  IO.read('public/my-page.html')
+end
+
+get '/:page' do
+  "You're on page " + params['page']
+end
