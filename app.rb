70d2d4cc9440147ca718e3ce3a64d0d16e181cb0
Esko Luontola <esko.luontola@gmail.com>
2015-10-03 00:46:02 +0300

    Parameterizing the page title

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 require 'sinatra'
 
 get '/' do
-  erb :index
+  erb :index, :locals => {:title => "I'm Ruby"}
 end
 
 get '/pictures.html' do
   picture_urls = Dir.glob('public/pictures/**').map { |path| path.sub('public', '') }
-  erb :pictures, :locals => {:picture_urls => picture_urls}
+  erb :pictures, :locals => {:picture_urls => picture_urls, :title => "Lovely Pictures"}
 end
 
 get '/:page.html' do
-  erb params['page'].to_sym
+  erb params['page'].to_sym, :locals => {:title => '???'}
 end
