e27600012a588173841e88d9802405b58bcb0707
Esko Luontola <esko.luontola@gmail.com>
2015-10-03 00:38:41 +0300

    Parameterize the template with a list of pictures

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ get '/' do
   erb :index
 end
 
+get '/pictures.html' do
+  picture_urls = [
+    "http://www.publicdomainpictures.net/pictures/50000/t2/cat-looking-up.jpg",
+    "http://www.publicdomainpictures.net/pictures/30000/t2/cat-in-the-city-5.jpg",
+    "http://www.publicdomainpictures.net/pictures/30000/t2/annoyed-cat.jpg",
+    "http://www.publicdomainpictures.net/pictures/30000/t2/cat-in-the-city-2.jpg",
+    "http://www.publicdomainpictures.net/pictures/50000/t2/cat-in-chair.jpg",
+    "http://www.publicdomainpictures.net/pictures/30000/t2/cat-hunting.jpg",
+    "http://www.publicdomainpictures.net/pictures/10000/t2/lieblingskater-44421287869401VYcy.jpg",
+    "http://www.publicdomainpictures.net/pictures/60000/t2/cat-1382017414PaD.jpg",
+    "http://www.publicdomainpictures.net/pictures/50000/t2/cat-1373445873hvw.jpg",
+    "http://www.publicdomainpictures.net/pictures/40000/t2/cross-eyed-cat.jpg",
+  ]
+  erb :pictures, :locals => {:picture_urls => picture_urls}
+end
+
 get '/:page.html' do
   erb params['page'].to_sym
 end
