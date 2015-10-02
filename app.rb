b81b3ce39bf919cd7b88c890f45bf212cac637c0
Esko Luontola <esko.luontola@gmail.com>
2015-10-03 00:58:20 +0300

    Sitemap for navigation

--- app.rb
+++ app.rb
@@ -7,15 +7,15 @@ PAGES = {
 }
 
 get '/' do
-  erb :index, :locals => {:title => PAGES[:index]}
+  erb :index, :locals => {:title => PAGES[:index], :pages => PAGES}
 end
 
 get '/pictures.html' do
   picture_urls = Dir.glob('public/pictures/**').map { |path| path.sub('public', '') }
-  erb :pictures, :locals => {:picture_urls => picture_urls, :title => PAGES[:pictures]}
+  erb :pictures, :locals => {:picture_urls => picture_urls, :title => PAGES[:pictures], :pages => PAGES}
 end
 
 get '/:page.html' do
   page = params['page'].to_sym
-  erb page, :locals => {:title => PAGES[page]}
+  erb page, :locals => {:title => PAGES[page], :pages => PAGES}
 end
