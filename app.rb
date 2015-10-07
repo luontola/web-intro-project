674dc1e71ae3d1590821befe2fb5a8958a8b2742
Esko Luontola <esko@luontola.fi>
2015-10-07 20:22:54 +0300

    Write comments to a database

--- app.rb
+++ app.rb
@@ -44,6 +44,12 @@ get '/pictures/:picture.html' do
 end
 
 post '/add-comment' do
+  Comment.create(
+    :picture => params['picture'],
+    :author => params['author'],
+    :message => params['message'],
+    :added => DateTime.now
+  )
   $comments << {
     :picture => params['picture'],
     :author => params['author'],
